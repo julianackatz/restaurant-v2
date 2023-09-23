cecbeae2b6be389fa89fc712930804ee549743eb
Juliana Katz <julianalimacunha@hotmail.com>
2023-09-23 16:03:30 -0400

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "164343107bfdf4ee41f0";
+/******/ 	var hotCurrentHash = "54d910e853b20b777290";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
